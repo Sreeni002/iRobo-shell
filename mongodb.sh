81debec67e549c8592bfd53e9732b3280cf66c38
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-07 22:55:37 +0530

    Change mongo repo conf file

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
+sed -i -e 's|127.0.0.1|0.0.0.0' /etc/mongo.conf
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 #We need to update the mongodb port no to 0.0.0.0
-systemctl restart mongod
