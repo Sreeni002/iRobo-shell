8c75fb8d2d3d2c543a915ba2f46bc957e6e12789
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-08 21:27:53 +0530

    giving input to the funcs using $1,$2 such

--- common.sh
+++ common.sh
@@ -2,34 +2,38 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
+print_head() {
+echo -e "\e[36m>>>>>>>>>>>>>>$1<<<<<<<<<<<<<<<<\e[0m"
+}
+
 function_nodejs() {
-echo -e "\e[36m>>>>>>>>>>>>>>Configuring nodeJS repos<<<<<<<<<<<<<<<<\e[0m"
+print_head "Configuring nodeJS repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[36m>>>>>>>>>>>>>>Inastall nodejs<<<<<<<<<<<<<<<<\e[0m"
+print_head "Inastall nodejs"
 yum install nodejs -y
 
-echo -e "\e[36m>>>>>>>>>>>>>>Add app user<<<<<<<<<<<<<<<<\e[0m"
+print_head "Add app user"
 useradd ${app_user}
 
-echo -e "\e[36m>>>>>>>>>>>>>>Create app Dir<<<<<<<<<<<<<<<<\e[0m"
+print_head "Create app Dir"
 rm -rf /app
 mkdir /app
 
-echo -e "\e[36m>>>>>>>>>>>>>>Download app content<<<<<<<<<<<<<<<<\e[0m"
+print_head "Download app content"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
 cd /app
 
-echo -e "\e[36m>>>>>>>>>>>>>>Extract the content<<<<<<<<<<<<<<<<\e[0m"
+print_head "Extract the content"
 unzip /tmp/${component}.zip
 
-echo -e "\e[36m>>>>>>>>>>>>>>Install npm repos<<<<<<<<<<<<<<<<\e[0m"
+print_head "Install npm repos"
 npm install
 
-echo -e "\e[36m>>>>>>>>>>>>>>Copy cart service file <<<<<<<<<<<<<<<<\e[0m"
+print_head "Copy cart service file"
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-echo -e "\e[36m>>>>>>>>>>>>>>Restart the cart service<<<<<<<<<<<<<<<<\e[0m"
+print_head "Restart the cart service"
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl restart ${component}
