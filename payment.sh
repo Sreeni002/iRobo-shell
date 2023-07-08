3a6431a6380f4668d6dba23bf03892c947348e8c
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-08 13:35:52 +0530

    setup payment service

--- payment.sh
+++ payment.sh
@@ -1,11 +1,27 @@
+echo -e "\e[36m>>>>>>>>>>>>>>Install python3<<<<<<<<<<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>Add application user<<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>>>>>>Create app Dir<<<<<<<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>>>>>>Download app content<<<<<<<<<<<<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+
+echo -e "\e[36m>>>>>>>>>>>>>>Extract the app content<<<<<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/payment.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>>>>>>>Install pip dependencies<<<<<<<<<<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
+
+echo -e "\e[36m>>>>>>>>>>>>>>Copy the systemd service file <<<<<<<<<<<<<<<<\e[0m"
+cp /home/centos/iRobo-shell/payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[36m>>>>>>>>>>>>>>Restart the payment service<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
-systemctl start payment
+systemctl restart payment
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,16 @@
+echo -e "\e[36m>>>>>>>>>>>>>>setup erlang repos<<<<<<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+echo -e "\e[36m>>>>>>>>>>>>>>setup rabbitmq repos<<<<<<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-yum install rabbitmq-server -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>Install erlang and rabbitmq<<<<<<<<<<<<<<<<\e[0m"
+yum install erlang rabbitmq-server -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>Restart rabbitmq server<<<<<<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
+
+echo -e "\e[36m>>>>>>>>>>>>>>Add application user in rabbitmq <<<<<<<<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
