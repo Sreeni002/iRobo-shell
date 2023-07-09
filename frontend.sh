52064dc303df601952630eef3052155d1e997e9a
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-09 22:31:15 +0530

    Setup frontend code in DRY

--- frontend.sh
+++ frontend.sh
@@ -2,11 +2,28 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
-systemctl enable nginx
+print_head "Install nginx"
+yum install nginx -y &>>$log_file
+func_status_check
+
+print_head "Copy Roboshop config file"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_status_check
+
+print_head "Remove the existing content"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+func_status_check
+
+print_head "Download the application content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+func_status_check
+
+print_head "Extract the App content"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+func_status_check
+
+print_head "Enable and restart the service"
+systemctl restart nginx &>>$log_file
+systemctl enable nginx &>>$log_file
+func_status_check
