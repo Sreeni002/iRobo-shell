27ce1a970e5c3b5b35787ba50ec3a64145c0636b
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-09 21:36:46 +0530

    Adding msgs to log file

--- common.sh
+++ common.sh
@@ -1,9 +1,13 @@
 app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
+log_file=/tmp/roboshop.log
+# the below command will delete the exisiting file and it will create new when the script runs every time
+# rm -rf $log_file
 
 print_head() {
 echo -e "\e[36m>>>>>>>>>>>>>>$1<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>$1<<<<<<<<<<<<<<<<\e[0m" &>>$log_file
 }
 
 func_status_check() {
@@ -11,79 +15,80 @@ func_status_check() {
       echo -e "\e[32m SUCCESS\e[0m"
     else
       echo -e "\e[32m FAILURE\e[0m"
+      echo "Refer the log file /tmp/roboshop.log for more info"
       exit 1
     fi
 }
 schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
     print_head "Copy mongo repo file"
-    cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
+    cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
     func_status_check $?
 
     print_head "Install mongo client"
-    yum install mongodb-org-shell -y
+    yum install mongodb-org-shell -y &>>$log_file
     func_status_check $?
 
     print_head "Load schema"
-    mongo --host mongodb-dev.sreenivasulareddydevops.online </app/schema/${component}.js
+    mongo --host mongodb-dev.sreenivasulareddydevops.online </app/schema/${component}.js &>>$log_file
     func_status_check $?
   fi
   if [ "${schema_setup}" == "mysql" ]; then
     print_head "Install my sql"
-    yum install mysql -y
+    yum install mysql -y &>>$log_file
     func_status_check $?
 
     print_head "Load schema"
-    mysql -h mysql-dev.sreenivasulareddydevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+    mysql -h mysql-dev.sreenivasulareddydevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file
     func_status_check $?
   fi
 }
 
 func_app_prereq() {
   print_head "Add app userid"
-  useradd ${app_user}
+  useradd ${app_user} &>>$log_file
   func_status_check $?
 
   print_head "create app directory"
-  rm -rf /app
-  mkdir /app
+  rm -rf /app &>>$log_file
+  mkdir /app &>>$log_file
   func_status_check $?
 
   print_head "Dowload the app content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
   func_status_check $?
 
   print_head "Extract the app content"
-  cd /app
-  unzip /tmp/${component}.zip
+  cd /app &>>$log_file
+  unzip /tmp/${component}.zip &>>$log_file
   func_status_check $?
 }
 
 func_systemd_setup() {
   print_head "Setup systemd service"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
   func_status_check $?
 
   print_head "Restart ${component} service"
   systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  systemctl enable ${component} &>>$log_file
+  systemctl restart ${component} &>>$log_file
   func_status_check $?
 }
 
 function_nodejs() {
   print_head "Configuring nodeJS repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
   func_status_check $?
 
 print_head "Inastall nodejs"
-  yum install nodejs -y
+  yum install nodejs -y &>>$log_file
   func_status_check $?
 
   func_app_prereq
 
   print_head "Install nodejs repos"
-  npm install
+  npm install &>>$log_file
   func_status_check $?
 
   schema_setup
@@ -92,17 +97,17 @@ print_head "Inastall nodejs"
 
 func_java() {
   print_head "Install Maven"
-  yum install maven -y
+  yum install maven -y >/tmp/roboshop.log
   func_status_check $?
 
   func_app_prereq
 
 print_head "Download Maven dependencies"
-  mvn clean package
+  mvn clean package &>>$log_file
 
   func_status_check $?
 
-  mv target/${component}-1.0.jar ${component}.jar
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
   schema_setup
   func_systemd_setup
 }
